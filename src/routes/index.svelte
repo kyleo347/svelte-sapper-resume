dfbfdfe68837b17f9b8053cc63cebe3218056f59
Kyle O'Connor <kyle.oconnor@slalom.com>
2022-01-06 16:38:33 -0800

    change theme and home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,31 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+  import profilePic from "images/hammahamma.jpg";
+  import Card from "@smui/card";
 </script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Kyle O'Connor</title>
 </svelte:head>
+<div>
+  <Card variant="outlined" padded style="display:flex;flex-direction:row">
+    <img alt="Kyle" src={profilePic} />
+    <div>
+      <h2>Kyle O'Connor</h2>
+      <p>Fullstack Developer</p>
+    </div>
+  </Card>
+</div>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<style>
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    float: left;
+    margin: 0 1rem;
+  }
+  .card {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
